45d4a69f417e3760ea8077fe5548816b332a0216
rheasukthanker <20137590+rheasukthanker@users.noreply.github.com>
2021-07-31 10:33:19 +0530

    Add files via upload

--- ACGAN_ALI_pretrained/V1/layers.py
+++ ACGAN_ALI_pretrained/V1/layers.py
@@ -1,6 +1,7 @@
 import tensorflow.keras.backend as K
 from keras.engine.topology import Layer
 
+
 class ConvMaxout(Layer):
     def __init__(self, n_piece, **kwargs):
         self.n_piece = n_piece
@@ -11,10 +12,10 @@ class ConvMaxout(Layer):
         h = K.shape(x)[1]
         w = K.shape(x)[2]
         ch = K.shape(x)[3]
-        x = K.reshape(x, (n, h, w, ch//self.n_piece, self.n_piece))
+        x = K.reshape(x, (n, h, w, ch // self.n_piece, self.n_piece))
         x = K.max(x, axis=-1)
         return x
 
     def compute_output_shape(self, input_shape):
         n, h, w, ch = input_shape
-        return (n, h, w, ch//self.n_piece)
+        return (n, h, w, ch // self.n_piece)
