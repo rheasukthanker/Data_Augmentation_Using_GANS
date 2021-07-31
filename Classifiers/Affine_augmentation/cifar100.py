f7704d4727c2b81c15ead045597e4c5575ffe221
rheasukthanker <20137590+rheasukthanker@users.noreply.github.com>
2021-07-31 10:41:11 +0530

    Add files via upload

--- Classifiers/Affine_augmentation/cifar100.py
+++ Classifiers/Affine_augmentation/cifar100.py
@@ -7,27 +7,32 @@ import numpy as np
 from sklearn.utils import class_weight
 from sklearn.metrics import confusion_matrix
 from keras.utils import np_utils
-def generate_imbalance(trainX,trainy,class_cur,perc):
-	class_to_sparsify=class_cur
-	inds_class=np.argwhere(trainy==class_to_sparsify)
-	num_delete=int((1-perc)*len(inds_class))
-	index = np.random.RandomState(seed=42).choice(inds_class.shape[0], num_delete, replace=False)
-	index_class=inds_class[index]
-	trainX_sparsified=np.delete(trainX,index_class,axis=0)
-	trainy_sparsified=np.delete(trainy,index_class)
-	return trainX_sparsified,trainy_sparsified
+
+
+def generate_imbalance(trainX, trainy, class_cur, perc):
+    class_to_sparsify = class_cur
+    inds_class = np.argwhere(trainy == class_to_sparsify)
+    num_delete = int((1 - perc) * len(inds_class))
+    index = np.random.RandomState(seed=42).choice(inds_class.shape[0],
+                                                  num_delete,
+                                                  replace=False)
+    index_class = inds_class[index]
+    trainX_sparsified = np.delete(trainX, index_class, axis=0)
+    trainy_sparsified = np.delete(trainy, index_class)
+    return trainX_sparsified, trainy_sparsified
+
+
 batch_size = 64
 epochs = 100
 IMG_HEIGHT = 32
 IMG_WIDTH = 32
-NUM_CHANNELS=3
-NUM_CLASSES=100
-latent_dim=64
-num_samples=20
+NUM_CHANNELS = 3
+NUM_CLASSES = 100
+latent_dim = 64
+num_samples = 20
 model = Sequential()
 
-model.add(Conv2D(128, (3, 3), padding='same',
-                 input_shape=(32,32,3)))
+model.add(Conv2D(128, (3, 3), padding='same', input_shape=(32, 32, 3)))
 model.add(Activation('elu'))
 model.add(Conv2D(128, (3, 3)))
 model.add(Activation('elu'))
@@ -48,7 +53,6 @@ model.add(Activation('elu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-
 model.add(Flatten())
 model.add(Dense(1024))
 model.add(Activation('elu'))
@@ -61,32 +65,56 @@ opt = tf.keras.optimizers.RMSprop(lr=0.0001, decay=1e-6)
 
 model.compile(loss='sparse_categorical_crossentropy',
               optimizer=opt,
-              metrics=['accuracy','sparse_top_k_categorical_accuracy'])
+              metrics=['accuracy', 'sparse_top_k_categorical_accuracy'])
 
 #CIFAR 100
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-mcp_save = tf.keras.callbacks.ModelCheckpoint('best.mdl_wts_cifar100_2.hdf5', save_best_only=True, monitor='val_loss', mode='min')
-cifar100=tf.keras.datasets.cifar100
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                  patience=10)
+mcp_save = tf.keras.callbacks.ModelCheckpoint('best.mdl_wts_cifar100_2.hdf5',
+                                              save_best_only=True,
+                                              monitor='val_loss',
+                                              mode='min')
+cifar100 = tf.keras.datasets.cifar100
 
 (train_images, train_labels), (test_images, test_labels) = cifar100.load_data()
-train_images = train_images/ 255.0
-test_images = test_images/255.0
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
-classes_to_sparsify=[83,69,35,34,2,90,37,49,23,26,41,62,33,12,84,50,57,74,93,40]
-sparsify_percentage=[0.65,0.65,0.65,0.65,0.65,0.7,0.7,0.7,0.7,0.7,0.75,0.75,0.75,0.75,0.75,0.8,0.8,0.8,0.8,0.8]
+classes_to_sparsify = [
+    83, 69, 35, 34, 2, 90, 37, 49, 23, 26, 41, 62, 33, 12, 84, 50, 57, 74, 93,
+    40
+]
+sparsify_percentage = [
+    0.65, 0.65, 0.65, 0.65, 0.65, 0.7, 0.7, 0.7, 0.7, 0.7, 0.75, 0.75, 0.75,
+    0.75, 0.75, 0.8, 0.8, 0.8, 0.8, 0.8
+]
 for i in range(len(classes_to_sparsify)):
-    train_images,train_labels=generate_imbalance(train_images,train_labels,classes_to_sparsify[i],sparsify_percentage[i])
+    train_images, train_labels = generate_imbalance(train_images, train_labels,
+                                                    classes_to_sparsify[i],
+                                                    sparsify_percentage[i])
 
-train_images,val_images,train_labels,val_labels = train_test_split(train_images,train_labels, test_size=0.1)
-datagen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=90,
-                 width_shift_range=0.1, height_shift_range=0.1,
-                 horizontal_flip=True)
+train_images, val_images, train_labels, val_labels = train_test_split(
+    train_images, train_labels, test_size=0.1)
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=90,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True)
 datagen.fit(train_images)
 
-class_weights = class_weight.compute_class_weight('balanced',np.unique(train_labels),np.squeeze(train_labels))
-model.fit_generator(datagen.flow(train_images, train_labels, batch_size=batch_size),class_weight=class_weights,callbacks=[early_stopping, mcp_save],validation_data=(val_images,val_labels), epochs=epochs)
+class_weights = class_weight.compute_class_weight('balanced',
+                                                  np.unique(train_labels),
+                                                  np.squeeze(train_labels))
+model.fit_generator(datagen.flow(train_images,
+                                 train_labels,
+                                 batch_size=batch_size),
+                    class_weight=class_weights,
+                    callbacks=[early_stopping, mcp_save],
+                    validation_data=(val_images, val_labels),
+                    epochs=epochs)
 model.load_weights("best.mdl_wts_cifar100_2.hdf5")
-test_loss, test_acc,c= model.evaluate(test_images,test_labels,verbose=2)
+test_loss, test_acc, c = model.evaluate(test_images, test_labels, verbose=2)
 print('\nTest accuracy:', test_acc)
-labels=model.predict(test_images)
-print("Confusion matrix",confusion_matrix(test_labels.argmax(axis=1), labels.argmax(axis=1)))
+labels = model.predict(test_images)
+print("Confusion matrix",
+      confusion_matrix(test_labels.argmax(axis=1), labels.argmax(axis=1)))
--- Classifiers/Affine_augmentation/fashion_mnist.py
+++ Classifiers/Affine_augmentation/fashion_mnist.py
@@ -6,28 +6,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import numpy as np
 from sklearn.utils import class_weight
+
+
 def top2_acc(labels, logits):
-    return tf.keras.metrics.sparse_top_k_categorical_accuracy(y_true=labels, y_pred=logits, k=2)
-def generate_imbalance(trainX,trainy,class_cur,perc):
-	class_to_sparsify=class_cur
-	inds_class=np.argwhere(trainy==class_to_sparsify)
-	num_delete=int((1-perc)*len(inds_class))
-	index = np.random.RandomState(seed=42).choice(inds_class.shape[0], num_delete, replace=False)
-	index_class=inds_class[index]
-	trainX_sparsified=np.delete(trainX,index_class,axis=0)
-	trainy_sparsified=np.delete(trainy,index_class)
-	return trainX_sparsified,trainy_sparsified
+    return tf.keras.metrics.sparse_top_k_categorical_accuracy(y_true=labels,
+                                                              y_pred=logits,
+                                                              k=2)
+
+
+def generate_imbalance(trainX, trainy, class_cur, perc):
+    class_to_sparsify = class_cur
+    inds_class = np.argwhere(trainy == class_to_sparsify)
+    num_delete = int((1 - perc) * len(inds_class))
+    index = np.random.RandomState(seed=42).choice(inds_class.shape[0],
+                                                  num_delete,
+                                                  replace=False)
+    index_class = inds_class[index]
+    trainX_sparsified = np.delete(trainX, index_class, axis=0)
+    trainy_sparsified = np.delete(trainy, index_class)
+    return trainX_sparsified, trainy_sparsified
+
+
 batch_size = 64
-epochs =100
-IMG_HEIGHT =28
+epochs = 100
+IMG_HEIGHT = 28
 IMG_WIDTH = 28
-NUM_CHANNELS=1
-NUM_CLASSES=10
-latent_dim=64
-num_samples=10000
-model= Sequential([
-    Conv2D(16, 3, padding='same', activation='relu',
-           input_shape=(IMG_HEIGHT, IMG_WIDTH ,NUM_CHANNELS)),
+NUM_CHANNELS = 1
+NUM_CLASSES = 10
+latent_dim = 64
+num_samples = 10000
+model = Sequential([
+    Conv2D(16,
+           3,
+           padding='same',
+           activation='relu',
+           input_shape=(IMG_HEIGHT, IMG_WIDTH, NUM_CHANNELS)),
     MaxPooling2D(),
     Dropout(0.2),
     Conv2D(32, 3, padding='same', activation='relu'),
@@ -42,40 +55,57 @@ model= Sequential([
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
-              metrics=['accuracy',top2_acc])
+              metrics=['accuracy', top2_acc])
 #FASHION MNIST
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-mcp_save = tf.keras.callbacks.ModelCheckpoint('best.mdl_wts_fashion_mnist.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                  patience=10)
+mcp_save = tf.keras.callbacks.ModelCheckpoint(
+    'best.mdl_wts_fashion_mnist.hdf5',
+    save_best_only=True,
+    monitor='val_loss',
+    mode='min')
 fashion_mnist = tf.keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
 #Normalize data
-train_images =train_images/255.
-test_images =test_images/255.
+train_images = train_images / 255.
+test_images = test_images / 255.
 #Fit model
-classes_to_sparsify=[1,3,7]# Define 3 random classes to sparsify
-sparsify_percentage=[0.5,0.65,0.8] #Amount by which classes are sparsified
+classes_to_sparsify = [1, 3, 7]  # Define 3 random classes to sparsify
+sparsify_percentage = [0.5, 0.65, 0.8]  #Amount by which classes are sparsified
 for i in range(len(classes_to_sparsify)):
-    train_images,train_labels=generate_imbalance(train_images,train_labels,classes_to_sparsify[i],sparsify_percentage[i])
-train_images=np.expand_dims(train_images,3)
-test_images=np.expand_dims(test_images,3)
-train_images,val_images,train_labels,val_labels = train_test_split(train_images,train_labels, test_size=0.1)
+    train_images, train_labels = generate_imbalance(train_images, train_labels,
+                                                    classes_to_sparsify[i],
+                                                    sparsify_percentage[i])
+train_images = np.expand_dims(train_images, 3)
+test_images = np.expand_dims(test_images, 3)
+train_images, val_images, train_labels, val_labels = train_test_split(
+    train_images, train_labels, test_size=0.1)
 #define data augmentor
-datagen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=10,
-            width_shift_range=0.1,
-            height_shift_range=0.1,
-            shear_range=0.3,
-            zoom_range=(0.9, 1.1),
-            horizontal_flip=False,
-            vertical_flip=False,
-            fill_mode='constant',
-            cval=0)
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.3,
+    zoom_range=(0.9, 1.1),
+    horizontal_flip=False,
+    vertical_flip=False,
+    fill_mode='constant',
+    cval=0)
 datagen.fit(train_images)
 
-class_weights = class_weight.compute_class_weight('balanced',np.unique(np.squeeze(train_labels)),np.squeeze(train_labels))
-model.fit_generator(datagen.flow(train_images, train_labels, batch_size=batch_size),callbacks=[early_stopping, mcp_save],class_weight=class_weights,validation_data=(val_images,val_labels), epochs=epochs)
+class_weights = class_weight.compute_class_weight(
+    'balanced', np.unique(np.squeeze(train_labels)), np.squeeze(train_labels))
+model.fit_generator(datagen.flow(train_images,
+                                 train_labels,
+                                 batch_size=batch_size),
+                    callbacks=[early_stopping, mcp_save],
+                    class_weight=class_weights,
+                    validation_data=(val_images, val_labels),
+                    epochs=epochs)
 model.load_weights("best.mdl_wts_fashion_mnist.hdf5")
 #Get test accuracy
-test_loss, test_acc,c= model.evaluate(test_images,test_labels,verbose=2)
-labels=model.predict(test_images)
-print("Confusion matrix",confusion_matrix(test_labels, labels.argmax(axis=1)))
+test_loss, test_acc, c = model.evaluate(test_images, test_labels, verbose=2)
+labels = model.predict(test_images)
+print("Confusion matrix", confusion_matrix(test_labels, labels.argmax(axis=1)))
 print('\nTest accuracy:', test_acc)
